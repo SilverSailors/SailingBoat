1765ca7ac45b3faf2b9d3430192018e5c5f887ca
Lucas Pentinsaari <kyusaki1@gmail.com>
2020-03-13 10:15:52 +0200

    Added additional script thing

--- install.sh
+++ install.sh
@@ -14,3 +14,6 @@ mkdir -p ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/settings/
 touch ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/settings/settings.txt
 touch ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/settings/sensor_config.txt
 touch ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/settings/destination.txt
+
+touch ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/logs/waypoint.txt
+touch ${XDG_CONFIG_HOME:-~/.config}/sailingBoat/logs/contest.txt
