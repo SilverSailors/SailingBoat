dbc9bb95e9b827ec7e6eecc807c38c39c69a1fca
Jonatan Björnvik <jonatan.bjornvik@ha.ax>
2020-03-20 16:27:58 +0200

    ModuleWindSensor bug-fixing

--- src/module_wind_sensor.cc
+++ src/module_wind_sensor.cc
@@ -18,6 +18,11 @@ bool ModuleWindSensor::Init() {
   return initialized_;
 }
 
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  ((std::string*)userp)->append((char*)contents, size * nmemb);
+  return size * nmemb;
+}
+
 void ModuleWindSensor::Run() {
   if (initialized_) {
     std::string data;
@@ -48,9 +53,4 @@ void ModuleWindSensor::Report() {
     std::cout << "-------------------" << std::endl;
     new_data_available_ = false;
   }
-}
-
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-  ((std::string*)userp)->append((char*)contents, size * nmemb);
-  return size * nmemb;
 }
